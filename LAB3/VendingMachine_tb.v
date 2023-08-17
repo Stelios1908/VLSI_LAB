0bb8a70edc0483c1146bc1db5842f5b502e965ae
Stylianos Sideris <siderisstil@gmail.com>
2023-08-17 18:49:19 +0300

    Update VendingMachine_tb.v
    ektyposeiw alla xwriw elegxoys

--- LAB3/VendingMachine_tb.v
+++ LAB3/VendingMachine_tb.v
@@ -6,6 +6,9 @@ reg clk,rst,c5,c10,NFC,beans;
 reg [4:0] water;
 wire error,coffee;
 
+integer i;
+integer trials=7;
+
 VendingMachine DUT(
                     .clk(clk),
                     .rst(rst),
@@ -23,9 +26,18 @@ VendingMachine DUT(
 
  always @(*)begin if(coffee) water = water-1; cnt_coffee=cnt_coffee+1; end
  always @(*)begin if(cnt_coffee==0) beans=0; end
+ always @(posedge clk) begin
+        #(period*0.30)
+        c5=0;
+        c10=0;
+        NFC=0;
+          
+  end
+
+
  initial begin 
 
- $monitor("state = %b |nextstate = %b|  coffee  = %b | ERROR = %b | c5 = %b | c10 =%b | NFC = %b | message = %b",DUT.state,DUT.nextstate,coffee,error,c5,c10,NFC,DUT.message);
+ $monitor("state = %b | coffee  = %b | ERROR = %b | c5 = %b | c10 =%b | NFC = %b | water  = %d | beans  = %d",DUT.state,coffee,error,c5,c10,NFC,water,beans);
     clk =0;
     rst=0;
     c5=0;
@@ -39,45 +51,54 @@ VendingMachine DUT(
     #(period*0.7);
     
    
+    for(i=0; i<trials; i=i+1) begin 
+        //pame gia  kafe me dio c5
+        $display("===============================================================================");
+        $display("===================== C5 AND C5 ===============================================");
+        c5=1;
+        #(period*2);
+        c5=1;
+        #(period);
+        #(period*6);
+    
+        //pame gia  kafe me ena c10
+        $display("===============================================================================");
+        $display("=====================     C10    ==============================================");
+        c10=1;
+        #(period);
+        #(period*6);
+        //pame gia  kafe me ena NFC
+        $display("===============================================================================");
+        $display("=====================   N F C   ===============================================");
+        NFC=1;
+        #(period);
+        #(period*6);
+        //pame gia  kafe me c5 kai meta  ena NFC
+        $display("===============================================================================");
+        $display("===================== C5 AND NFC ==============================================");
+        c5=1;
+        #(period);
+        #(period*3);
+        NFC=1;
+        #(period);
+        #(period*6);
+         //pame gia  kafe me c5 kai meta c10
+        $display("===============================================================================");
+        $display("===================== C5 AND C10 ==============================================");
+        c5=1;
+        #(period);
+        #(period*3);
+        c10=1;
+        #(period);
+        #(period*6);
 
-    //pame gia  kafe me dio c5
-    c5=1;
-    #(period);
-    c5=0;
-    #(period*2);
-    c5=1;
-    #(period);
-    #(period*6);
-    //pame gia  kafe me ena c10
-    c10=1;
-    #(period);
-    c10=0;
-    #(period*6);
-    //pame gia  kafe me ena NFC
-    NFC=1;
-    #(period);
-    NFC=0;
-    #(period*6);
-    //pame gia  kafe me c5 kai meta  ena NFC
-    c5=1;
-    #(period);
-    c5=0;
-    #(period*3);
-    NFC=1;
-    #(period);
-    NFC=0;
-    #(period*6);
-     //pame gia  kafe me c5 kai meta c10
-    c5=1;
-    #(period);
-    c5=0;
-    #(period*3);
-    c10=1;
-    #(period);
-    c10=0;
-    #(period*6);
-    $finish;
- end
+        if(i==3) begin  water=20; end
+        if(i==4) begin beans=0;   end
+        else     begin beans=1;   end
+    end  //end for
+$finish;
+    
+end
 
  initial begin
    $dumpfile("dump.vcd");
