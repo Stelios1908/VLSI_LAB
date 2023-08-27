a604284640a18caad4511813c3b5dae440d36941
Stylianos Sideris <siderisstil@gmail.com>
2023-08-27 21:42:56 +0300

    Update lockcomp.v

--- LAB3/lockcomp.v
+++ LAB3/lockcomp.v
@@ -2,7 +2,7 @@ module lockcomp#(
    parameter size=16,
    parameter [size-1:0]factory_pass = {size{16'b0}}
 )(
-   input rst,prs,
+   input rstn,prs,
    input  [size-1:0]incode,
    output reg equal
 );
@@ -10,14 +10,18 @@ module lockcomp#(
 
 reg [size-1:0]code_for_cmp;
 
-always @(incode or code_for_cmp or rst or prs)
+always @(incode or code_for_cmp or rstn or prs)
  begin 
-   if(rst) code_for_cmp = factory_pass; 
+   if(!rstn) code_for_cmp = factory_pass; 
    else if(prs) code_for_cmp = incode;
    else begin
-          if(incode == code_for_cmp) equal=1;
-          else equal=0;
+          if(incode == code_for_cmp) begin
+             equal =1;
+             end
+          else begin
+              equal =0;
+          end
         end
-end  
+  end  
 
 endmodule
