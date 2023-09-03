786a5d8a27f0c77563d12c301191af6ae047b19a
Stylianos Sideris <siderisstil@gmail.com>
2023-09-03 20:27:12 +0300

    Update rca.v

--- LAB4/src/rca.v
+++ LAB4/src/rca.v
@@ -16,46 +16,38 @@ module rca
   assign temp_c[0] = 1'b0;
   
   
-  genvar gi;
+  genvar gi, ti;
   generate
-    for (gi=0; gi<width; gi=gi+1) begin: rega_regb_fas
-
-     myreg reg_a  (
-                    .clk(clk),
-                    .rst(rst),
-                    .D(a_i[gi]),
-                    .Q(temp_a[gi])
-                  );
-     myreg reg_b  (
-                    .clk(clk),
-                    .rst(rst),
-                    .D(a_i[gi]),
-                    .Q(temp_b[gi])
-                  );
-   
-	   fulladder fa (
-                    .a(temp_a[gi]),
-                    .b(temp_b[gi]),
-                    .cin(temp_c[gi]),
-                    .s(temp_sum[gi]),
-                    .cout(temp_c[gi+1])
-                  );
-	end
+         for (gi=0; gi<width; gi=gi+1) begin: rega_regb_fas
+               myreg reg_a (
+                         .clk(clk),
+                         .rst(rst),
+                         .D(a_i[gi]),
+                         .Q(temp_a[gi])
+                           );
+               myreg reg_b (
+                         .clk(clk),
+                         .rst(rst),
+                         .D(a_i[gi]),
+                         .Q(temp_b[gi])
+                           );
+        
+	             fulladder fa (
+                         .a(temp_a[gi]),
+                         .b(temp_b[gi]),
+                         .cin(temp_c[gi]),
+                         .s(temp_sum[gi]),
+                         .cout(temp_c[gi+1])
+                         );
+	       end
+         for(ti=0;ti<width+1;ti=ti+1) begin: reg_sum
+               myreg reg_sum(
+                         .clk(clk),
+                         .rst(rst),
+                         .D(temp_sum[ti]),
+                         .Q(sum_o[ti])
+                        );
+         end
   endgenerate
-
-  genvar ti;
-  generate 
-     for(ti=0;ti<width+1;ti=ti+1) begin: reg_sum
-         if(ti<width) begin  
-           myreg reg_sum(
-                    .clk(clk),
-                    .rst(rst),
-                    .D(temp_sum[ti]),
-                    .Q(sum_o[ti])
-                   );
-        end
-     end
-  endgenerate
-  
   assign temp_sum= {temp_c[width],temp_sum[width-1:0]};
 endmodule
