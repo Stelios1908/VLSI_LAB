e2d74861e353d5729b1e4840053339c4126577ba
Stylianos Sideris <siderisstil@gmail.com>
2023-09-20 00:26:09 +0300

    Update mycell.v

--- LAB5/src/ArtificialNeuron/mycell.v
+++ LAB5/src/ArtificialNeuron/mycell.v
@@ -3,10 +3,10 @@ module mycell #(
     parameter n=8,  //length weigth
     parameter k=8  //length activation
 )(
-    input  [k-1:0] activation,
-    input  [n-1:0] weigth,
-    input  [k+n+$clog2(m)-1:0]sum_in,
-    output  [k+n+$clog2(m)-1:0] sum_out
+    input  signed [k-1:0] activation,
+    input  signed[n-1:0] weigth,
+    input  signed[k+n+$clog2(m)+1:0]sum_in,
+    output signed[k+n+$clog2(m)+1:0] sum_out
 );
 
 assign sum_out = activation*weigth + sum_in;
